186b059cdb36aa3ae5f7de07d0ecf0d0dffeed60
Karun Binny <95923091+lykanranger@users.noreply.github.com>
2025-08-11 20:05:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,29 +9,25 @@ const formEl = document.getElementById("form");
 
 const scoreEl = document.getElementById("score");
 
-let score = JSON.parse(localStorage.getItem("score"));
-
-if (!score) {
-  score = 0;
-}
+let score = 0;
+let correctAns = num1 * num2;
 
 scoreEl.innerText = `score: ${score}`;
 
 questionEl.innerText = `What is ${num1} multiplied by ${num2}?`;
 
-const correctAns = num1 * num2;
-
-formEl.addEventListener("submit", () => {
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
   const userAns = +inputEl.value;
   if (userAns === correctAns) {
     score++;
-    updateLocalStorage();
   } else {
     score--;
-    updateLocalStorage();
   }
+  scoreEl.innerText = `score: ${score}`;
+  inputEl.value = "";
+  const newNum1 = Math.ceil(Math.random() * 10);
+  const newNum2 = Math.ceil(Math.random() * 10);
+  correctAns = newNum1 * newNum2; // Update correctAns with new numbers
+  questionEl.innerText = `What is ${newNum1} multiplied by ${newNum2}?`;
 });
-
-function updateLocalStorage() {
-  localStorage.setItem("score", JSON.stringify(score));
-}
